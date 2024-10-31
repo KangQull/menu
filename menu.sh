f7c882addb95261544388d8917aba1225903a6c7
KangQull <110822952+KangQull@users.noreply.github.com>
2024-10-31 11:53:54 +0700

    Create menu.sh

---
+++ menu.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#                                                                          # CREATED By  kangqull
+#
+echo "*******************************************"
+echo "*                                         *"
+echo "*     Daftar menu Menu yang kamu mau      *"
+echo "*     ______________________________      *"
+echo "*                                         *"
+echo "* 1) Rubah Windows    3) Hapus windows    *"
+echo "* 2) Matikan Windows  4) Nyalakan windows *"
+echo "*                                         *"
+echo "*******************************************"
+
+read -p "Pilih menu [1]: " menus
+
+case "$menus" in
+        1) bash menu/rubah.sh
+        ;;
+        2) bash helo.sh
+        ;;
+        *) echo "pilihan salah"; exit;;
+esac
